066e3bdb4e9da97a9e09f818227ab1539396bafa
salomao-neto <salomao.cruz.15@gmail.com>
2024-04-27 11:16:10 -0300

    feat: create user entity

--- src/main/kotlin/com/salomao_neto/financial_server/domain/user/UserEntity.kt
+++ src/main/kotlin/com/salomao_neto/financial_server/domain/user/UserEntity.kt
@@ -2,4 +2,8 @@ package com.salomao_neto.financial_server.domain.user
 
 import java.util.*
 
-class UserEntity(var id: UUID? = null, var email: String = "", var password: String = "", var name: String = "") {}
+data class UserEntity(
+    var id: UUID? = null,
+    var email: String? = "",
+    var name: String? = ""
+) {}
