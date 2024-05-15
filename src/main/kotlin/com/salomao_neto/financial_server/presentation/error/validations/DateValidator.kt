a81af3013f7d34b81f618ff493189a1f605435a1
salomao-neto <salomao.cruz.15@gmail.com>
2024-05-15 20:00:21 -0300

    feat: create date validator

---
+++ src/main/kotlin/com/salomao_neto/financial_server/presentation/error/validations/DateValidator.kt
@@ -0,0 +1,30 @@
+package com.salomao_neto.financial_server.presentation.error.validations
+
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [DateValidator::class])
+annotation class Date(
+    val message: String = "This date is invalid",
+    val groups: Array<KClass<Any>> = [],
+    val payload: Array<KClass<Payload>> = []
+)
+
+
+class DateValidator : ConstraintValidator<Date, String> {
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        try {
+            Instant.from(DateTimeFormatter.ISO_INSTANT.parse(value))
+            return true
+        } catch (e: Throwable) {
+            return false
+        }
+    }
+}
