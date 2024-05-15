1b7cbef95d2da4c9e97da6eda203d885c1884192
salomao-neto <salomao.cruz.15@gmail.com>
2024-05-15 20:32:04 -0300

    feat: added findByUserId to transaction database

--- src/main/kotlin/com/salomao_neto/financial_server/infra/financial/database/TransactionDatabase.kt
+++ src/main/kotlin/com/salomao_neto/financial_server/infra/financial/database/TransactionDatabase.kt
@@ -1,8 +1,13 @@
 package com.salomao_neto.financial_server.infra.financial.database
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface TransactionDatabase : JpaRepository<Transaction, UUID>
+interface TransactionDatabase : JpaRepository<Transaction, UUID> {
+
+    @Query(value = "SELECT * FROM 'financial-transaction' WHERE user_id = ?", nativeQuery = true)
+    fun findByUserId(userId: UUID): List<Transaction>
+}
